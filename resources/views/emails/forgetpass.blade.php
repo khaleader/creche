4463d446f46a3d02384393d8281a9d1872f5d426
khaleader <khaleader1@gmail.com>
2016-01-04 12:58:04 +0000

    about inscription

--- resources/views/emails/forgetpass.blade.php
+++ resources/views/emails/forgetpass.blade.php
@@ -1,3 +1,10 @@
-<h3>Bienvenue {{   'Votre Email est '. $email }}</h3>
-<h2>Votre Nouveau password est : {{ $new_password }} </h2>
+<h1>Oblivius Petite Enfance</h1>
+<strong>Vous avez oublié votre Mot de Pass ?</strong> <br>
+<h2>Nous avons recu un email de demande d'un nouveau Mot de Pass </h2>
+<h4>Vous pouvez Vous conncter en utilisant les identifiants suivants :</h4>
+<h3> {{  ' Email  '. $email }}</h3>
+<h2>Votre Nouveau Mot De pass est : {{ $new_password }} </h2>
+<h3>N'oubliez pas changer le mot de pass dès que vous connectez Pour ne pas l'oublier en cliquant sur votre Profile</h3>
+
+<a href="http://www.petite-enfance.oblivius.fr">Connectez vous en cliquant ici</a>
 
