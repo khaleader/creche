9c92493b57b6cb3c1b29dd6c6139ce2c798f5a2b
khaleader <khaleader1@gmail.com>
2016-01-13 15:54:22 +0000

    create and index

---
+++ database/migrations/2016_01_13_145608_create_timesheets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('lundi',20);
+            $table->string('mardi',20);
+            $table->string('mercredi',20);
+            $table->string('jeudi',20);
+            $table->string('vendredi',20);
+            $table->string('samedi',20);
+            $table->time('time');
+            $table->integer('user_id')->unsigned();
+            $table->integer('classroom_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+          Schema::drop('timesheets');
+    }
+}
