58c80340720a0ebd1ff5aa20bcb0bc9ef1c6b37a
khaleader <khaleader1@gmail.com>
2016-03-24 11:48:52 +0000

    head

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@ session_start();
 
 @stop
 @section('content')
-{{ str_plural('occasion') }}
+
     @if(\Auth::user()->isAdmin() && \Auth::user()->typeCompte == 0)
     <div class="row">
         <div class="col-md-12">
