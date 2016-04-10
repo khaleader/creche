dccc30629adda000e7d639a2eb2fd3233503feb9
khaleader <khaleader1@gmail.com>
2016-04-10 19:29:48 +0100

    huge

--- resources/views/occasions/show.blade.php
+++ resources/views/occasions/show.blade.php
@@ -246,7 +246,7 @@ $(function(){
         eventRender: function(event, element) {
             element.append("<span  style='display: inline-block' class='closeon'>X</span>");
             $(element).tooltip({
-                title: event.title + ' <br><i class="fa fa-clock-o fa-2x"> </i><br> '+
+                title: event.title + ' <br><i class="fa fa-clock-o fa-2x"> </i><br> &nbsp;&nbsp;'+
                 moment(event.start).format('LLL') + " Jusqu'au " +
                 moment(event.end).format('LLL'),
                 placement : 'bottom',
